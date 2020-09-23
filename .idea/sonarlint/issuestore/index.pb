7b68c031e69aa1b6b44fa2a664227967e136586a
Rashed Qazizada <rashidqk@gmail.com>
2020-09-23 12:58:12 +0200

    data structure package added

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -7,3 +7,15 @@ N
 src/algo/linearSearch/App.java,f\3\f31b6d502761cffac782635b1d57f572a480415f
 L
 src/algo/recurssion/App.java,3\6\36c794dbcf3199ea7ca44093266a7e7b75e2c93a
+O
+src/algo/insertionsort/App.java,5\8\5897a3cdbf700592b43498b585e94883e26f2f43
+K
+src/algo/mergesort/App.java,3\7\37f64bda70f90df0f04cb57b6acdd7ab2f3a2165
+Q
+!src/algo/mergesort/MergeSort.java,1\6\16b0c44c4b18a49c24dfa79a3883f33906f733b5
+Y
+)src/algo/selectionsort/SelectionSort.java,1\e\1e515183abd35a6cfd81430c9576246ac8ae7844
+T
+$src/algo/quickSort/QuickSortApp.java,6\7\67520e623eb7e86694b79189395151bf449de6d9
+G
+src/algo/TestClass.java,3\e\3ea15f07e89f74eda28baae6f3e4a967c6f50d90
--- src/algo/TestClass.java
+++ src/algo/TestClass.java
@@ -2,7 +2,104 @@ package algo;
 
 public class TestClass {
     public static void main(String[] args) {
+        int x=2;
+        int y=3;
+        int n=10;
 
 
+
+
+
+        int[]myArr={1,2,3};//3
+        int i2 =4;
+
+
+
+        int[]newArr=new int[myArr.length*2];
+        System.out.println(newArr.length);
+
+        for(int i=0;i<myArr.length; i++){
+//            System.out.println(myArr[i]);
+            newArr[i]=myArr[i];
+
+//            myArr=newArr;
+
+//            newArr[3]=4;
+//            newArr[4]=5;
+        }
+        myArr=newArr;
+        myArr[4]=1000;
+
+        for (int a :
+                myArr) {
+            System.out.println(a);
+        }
+
+//
+//        for (int a:
+//             myArr) {
+//            System.out.println(a);
+//
+//        }
+//
+//        System.out.println("__________________________\n");
+//        int[]newArr=new int[myArr.length*2];
+//        System.out.println(myArr.length+"\n");
+//        for(int i=0;i<myArr.length; i++){
+//            newArr[i]=myArr[i];
+//
+//
+//        }
+//        System.out.println(newArr.length-myArr.length);
+//        System.out.println("\"aaa\"+");
+//        for(int i =newArr.length-myArr.length;i<newArr.length;i++){
+//            newArr[i]=i*5;
+//
+//        }
+//
+//        for (int b:
+//             newArr) {
+//            System.out.println(b);
+//        }
+
+//        System.out.println(sum(2));
+
+//        System.out.println(f(4));
+    }
+//    public static long pow( long x, int n )
+//    {
+//        if( n == 0 )
+//            return 1;
+//        if( n == 1 )
+//            return x;
+//        if( isEven( n ) )
+//            return pow( x * x, n / 2 );
+//        else
+//            return pow( x * x, n / 2 ) * x;
+//    }
+//    public static void printOut( int n )
+//    {
+//        if( n >= 10 )
+//            printOut( n / 10 );
+//        printDigit( n % 10 );
+//    }
+    public static int f( int x )
+    {
+
+        if( x == 0 )
+            return 0;
+        else
+
+            return 2 * f( x - 1 ) + x * x;
+    }
+
+
+    public static int sum( int n )
+    {
+        int partialSum;
+        partialSum = 0;
+        for( int i = 1; i <= n; i++ )
+            partialSum += i * i * i;
+        return partialSum;
     }
 }
